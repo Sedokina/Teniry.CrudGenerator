b8e08acc3df2435bf337c363d5576bf8373e3c96
Sedokina <sedokina52@gmail.com>
2024-09-14 15:28:25 +0600

    Generate list command

---
+++ src/Mars/Mars.Generators/GetListCommandGenerator.cs
@@ -0,0 +1,136 @@
+using System;
+using System.Text;
+using Mars.Generators.Extensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using Scriban;
+
+namespace Mars.Generators;
+
+[Generator]
+public class GetListCommandGenerator : ISourceGenerator
+{
+    private const string CommandResourcePath = "Mars.Generators.Templates.GetListCommand.txt";
+    private const string DtoListItemResourcePath = "Mars.Generators.Templates.GetListItemDto.txt";
+    private const string DtoResourcePath = "Mars.Generators.Templates.GetListDto.txt";
+    private const string HandlerResourcePath = "Mars.Generators.Templates.GetListHandler.txt";
+
+    public void Initialize(GeneratorInitializationContext context)
+    {
+        context.RegisterForSyntaxNotifications(() => new AttributeSyntaxReceiver<GenerateCreateCommandAttribute>());
+    }
+
+    public void Execute(GeneratorExecutionContext context)
+    {
+        if (context.SyntaxReceiver is not AttributeSyntaxReceiver<GenerateCreateCommandAttribute> syntaxReceiver)
+        {
+            return;
+        }
+
+        foreach (var classSyntax in syntaxReceiver.Classes)
+        {
+            // Converting the class to semantic model to access much more meaningful data.
+            var model = context.Compilation.GetSemanticModel(classSyntax.SyntaxTree);
+            // Parse to declared symbol, so you can access each part of code separately, such as interfaces, methods, members, contructor parameters etc.
+            var symbol = model.GetDeclaredSymbol(classSyntax) ?? throw new ArgumentException("symbol");
+
+            GenerateCommand(context, symbol);
+            GenerateListItemDto(context, symbol);
+            GenerateListDto(context, symbol);
+            GenerateHandler(context, symbol);
+        }
+    }
+
+
+    private void GenerateCommand(GeneratorExecutionContext context, ISymbol symbol)
+    {
+        var template = Template
+            .Parse(EmbeddedResourceExtensions.GetEmbeddedResource(CommandResourcePath, GetType().Assembly));
+
+        var sourceCode = template.Render(new
+        {
+            ClassName = symbol.Name,
+            Namespace = symbol.ContainingNamespace,
+            PreferredNamespace = symbol.ContainingAssembly.Name,
+        });
+
+        context.AddSource(
+            $"Get{symbol.Name}ListCommand.g.cs",
+            SourceText.From(sourceCode, Encoding.UTF8));
+    }
+
+    private void GenerateListItemDto(GeneratorExecutionContext context, ISymbol symbol)
+    {
+        var template = Template
+            .Parse(EmbeddedResourceExtensions.GetEmbeddedResource(DtoListItemResourcePath, GetType().Assembly));
+
+        var propertiesOfClass = ((INamedTypeSymbol)symbol).GetMembers().OfType<IPropertySymbol>();
+        var result = "";
+        foreach (var propertySymbol in propertiesOfClass)
+        {
+            // skip adding to command if not primitive type
+            if (!propertySymbol.Type.IsSimple())
+            {
+                continue;
+            }
+
+            // For DateTimeOffset and other date variations remove system from the property type declaration
+            var propertyTypeName = propertySymbol.Type.ToString().ToLower().StartsWith("system.")
+                ? propertySymbol.Type.MetadataName
+                : propertySymbol.Type.ToString();
+
+            result += $"public {propertyTypeName} {propertySymbol.Name} {{ get; set; }}\n\t";
+        }
+
+        result = result.TrimEnd();
+
+        var sourceCode = template.Render(new
+        {
+            ClassName = symbol.Name,
+            Namespace = symbol.ContainingNamespace,
+            PreferredNamespace = symbol.ContainingAssembly.Name,
+            Properties = result,
+        });
+
+        context.AddSource(
+            $"{symbol.Name}ListItemDto.g.cs",
+            SourceText.From(sourceCode, Encoding.UTF8));
+    }
+
+    private void GenerateListDto(GeneratorExecutionContext context, ISymbol symbol)
+    {
+        var template = Template
+            .Parse(EmbeddedResourceExtensions.GetEmbeddedResource(DtoResourcePath, GetType().Assembly));
+
+        var sourceCode = template.Render(new
+        {
+            ClassName = symbol.Name,
+            Namespace = symbol.ContainingNamespace,
+            PreferredNamespace = symbol.ContainingAssembly.Name,
+            ItemsType = $"{symbol.Name}ListItemDto"
+        });
+
+        context.AddSource(
+            $"{symbol.Name}ListDto.g.cs",
+            SourceText.From(sourceCode, Encoding.UTF8));
+    }
+
+    private void GenerateHandler(GeneratorExecutionContext context, ISymbol symbol)
+    {
+        var template = Template
+            .Parse(EmbeddedResourceExtensions.GetEmbeddedResource(HandlerResourcePath, GetType().Assembly));
+
+        var sourceCode = template.Render(new
+        {
+            EntityName = symbol.Name,
+            Namespace = symbol.ContainingNamespace,
+            PreferredNamespace = symbol.ContainingAssembly.Name,
+            CommandName = $"Get{symbol.Name}ListCommand",
+            DtoName = $"{symbol.Name}ListDto",
+        });
+
+        context.AddSource(
+            $"Get{symbol.Name}ListHandler.g.cs",
+            SourceText.From(sourceCode, Encoding.UTF8));
+    }
+}
