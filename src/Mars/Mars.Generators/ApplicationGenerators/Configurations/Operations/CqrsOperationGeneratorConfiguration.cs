b93149c24e0c3066e900b25b51d0639f9da08817
Sedokina <sedokina52@gmail.com>
2024-09-21 18:01:33 +0600

    Not working configuration build

--- src/Mars/Mars.Generators/ApplicationGenerators/Configurations/Operations/CqrsOperationGeneratorConfiguration.cs
+++ src/Mars/Mars.Generators/ApplicationGenerators/Configurations/Operations/CqrsOperationGeneratorConfiguration.cs
@@ -1,5 +1,6 @@
 using Mars.Generators.ApplicationGenerators.Configurations.Global;
 using Mars.Generators.ApplicationGenerators.Configurations.Operations.TypedConfigurations;
+using Mars.Generators.ApplicationGenerators.Core.EntitySchemaCore;
 
 namespace Mars.Generators.ApplicationGenerators.Configurations.Operations;
 
@@ -12,4 +13,52 @@ public class CqrsOperationGeneratorConfiguration
     public FileTemplateBasedOperationConfiguration Operation { get; set; }
     public FileTemplateBasedOperationConfiguration Handler { get; set; }
     public MinimalApiEndpointConfiguration Endpoint { get; set; }
+
+    protected virtual BuildConfiguration(EntityName entityName)
+    {
+        
+    }
+    
+    public CqrsOperationGeneratorConfigurationBuilt Build(EntityName entityName)
+    {
+        return new CqrsOperationGeneratorConfigurationBuilt
+        {
+            GlobalConfiguration = GlobalConfiguration,
+            OperationsSharedConfiguration = OperationsSharedConfiguration,
+            OperationType = CqrsOperationType.Command,
+            FunctionName = FunctionName.GetName(entityName),
+            Operation = new()
+            {
+                TemplatePath = Operation.TemplatePath,
+                Name = Operation.NameConfiguration.GetName(entityName),
+            },
+            Handler = new()
+            {
+                TemplatePath = Handler.TemplatePath,
+                Name = Handler.NameConfiguration.GetName(entityName),
+            },
+            Endpoint = new()
+            {
+                TemplatePath = Endpoint.TemplatePath,
+                Name = Endpoint.NameConfiguration.GetName(entityName),
+            }
+        };
+    }
+}
+
+public class CqrsOperationGeneratorConfigurationBuilt
+{
+    public GlobalCqrsGeneratorConfiguration GlobalConfiguration { get; set; }
+    public CqrsOperationsSharedConfiguration OperationsSharedConfiguration { get; set; }
+    public CqrsOperationType OperationType { get; set; }
+    public string FunctionName { get; set; }
+    public FileTemplateBasedOperationConfigurationBuilt Operation { get; set; }
+    public FileTemplateBasedOperationConfigurationBuilt Handler { get; set; }
+    public FileTemplateBasedOperationConfigurationBuilt Endpoint { get; set; }
+}
+
+public class FileTemplateBasedOperationConfigurationBuilt
+{
+    public string TemplatePath { get; set; }
+    public string Name { get; set; }
 }
--- src/Mars/Mars.Generators/ApplicationGenerators/Configurations/Operations/CqrsOperationWithReturnValueGeneratorConfiguration.cs
+++ src/Mars/Mars.Generators/ApplicationGenerators/Configurations/Operations/CqrsOperationWithReturnValueGeneratorConfiguration.cs
@@ -1,8 +1,37 @@
 using Mars.Generators.ApplicationGenerators.Configurations.Operations.TypedConfigurations;
+using Mars.Generators.ApplicationGenerators.Core.EntitySchemaCore;
 
 namespace Mars.Generators.ApplicationGenerators.Configurations.Operations;
 
 public class CqrsOperationWithReturnValueGeneratorConfiguration : CqrsOperationGeneratorConfiguration
 {
     public FileTemplateBasedOperationConfiguration Dto { get; set; }
+
+    public new CqrsOperationWithReturnValueGeneratorConfigurationBuilt Build(EntityName entityName)
+    {
+        base.Build()
+        var built = new CqrsOperationWithReturnValueGeneratorConfigurationBuilt();
+        built.Dto = new()
+        {
+            TemplatePath = Handler.TemplatePath,
+            Name = Handler.NameConfiguration.GetName(entityName),
+        };
+        return built;
+    }
+
+    protected override T Build<T>(EntityName entityName)
+    {
+        var built = base.Build<CqrsOperationWithReturnValueGeneratorConfigurationBuilt>(entityName);
+        built.Dto = new()
+        {
+            TemplatePath = Handler.TemplatePath,
+            Name = Handler.NameConfiguration.GetName(entityName),
+        };
+        return built;
+    }
+}
+
+public class CqrsOperationWithReturnValueGeneratorConfigurationBuilt : CqrsOperationGeneratorConfigurationBuilt
+{
+    public FileTemplateBasedOperationConfigurationBuilt Dto { get; set; }
 }
