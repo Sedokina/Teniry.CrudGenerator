8e10e041e734ae77eb1e7f1ba90257a9a56b963e
Sedokina <sedokina52@gmail.com>
2024-09-25 22:12:49 +0600

    Merge main

--- src/Mars/ITech.CrudGenerator/CrudGeneratorCore/OperationsGenerators/GetByIdQueryCrudGenerator.cs
+++ src/Mars/ITech.CrudGenerator/CrudGeneratorCore/OperationsGenerators/GetByIdQueryCrudGenerator.cs
@@ -1,8 +1,12 @@
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using ITech.CrudGenerator.CrudGeneratorCore.Configurations.Operations.BuiltConfigurations;
 using ITech.CrudGenerator.CrudGeneratorCore.OperationsGenerators.Core;
 using ITech.CrudGenerator.CrudGeneratorCore.Schemes.Entity.Formatters;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace ITech.CrudGenerator.CrudGeneratorCore.OperationsGenerators;
 
@@ -80,24 +84,98 @@ internal class
 
     private void GenerateEndpoint(string templatePath)
     {
-        var routeParams = EntityScheme.PrimaryKeys.FormatAsMethodDeclarationParameters();
-        var constructorParameters = EntityScheme.PrimaryKeys.FormatAsMethodCallArguments();
-        var model = new
-        {
-            EndpointClassName = _endpointClassName,
-            FunctionName = Scheme.Configuration.Endpoint.FunctionName,
-            QueryName = _queryName,
-            DtoName = _dtoName,
-            RouteParams = routeParams,
-            QueryConstructorParameters = constructorParameters
-        };
+        var endpointNamespace = Scheme.Configuration.OperationsSharedConfiguration.EndpointsNamespaceForFeature;
+        var businessLogicNamespace =
+            Scheme.Configuration.OperationsSharedConfiguration.BusinessLogicNamespaceForOperation;
+        var compilationUnit = SyntaxFactory.CompilationUnit();
+        compilationUnit = compilationUnit.AddUsings([
+            SyntaxFactory.UsingDirective(SyntaxFactory.ParseName("Microsoft.AspNetCore.Mvc")),
+            SyntaxFactory.UsingDirective(SyntaxFactory.ParseName("ITech.Cqrs.Cqrs.Queries")),
+            SyntaxFactory.UsingDirective(SyntaxFactory.ParseName(businessLogicNamespace))
+        ]);
 
-        WriteFile(templatePath, model, _endpointClassName);
+        var @namespace = SyntaxFactory
+            .FileScopedNamespaceDeclaration(SyntaxFactory.ParseName(endpointNamespace));
 
-        EndpointMap = new EndpointMap(EntityScheme.EntityName.ToString(),
-            Scheme.Configuration.OperationsSharedConfiguration.EndpointsNamespaceForFeature,
-            "Get",
-            Scheme.Configuration.Endpoint.Route,
-            $"{_endpointClassName}.{Scheme.Configuration.Endpoint.FunctionName}");
+        //  Create a class: (class Order)
+        var classDeclaration = SyntaxFactory.ClassDeclaration(_endpointClassName);
+
+        // Add the public modifier: (public class Order)
+        classDeclaration = classDeclaration.AddModifiers([
+            SyntaxFactory.Token(SyntaxKind.PublicKeyword),
+            SyntaxFactory.Token(SyntaxKind.StaticKeyword),
+            SyntaxFactory.Token(SyntaxKind.PartialKeyword)
+        ]);
+
+        // Create a stament with the body of a method.
+        var syntax = SyntaxFactory.ParseStatement("return TypedResults.Ok();");
+        // var assignment = SyntaxFactory.AssignmentExpression(SyntaxKind.VariableDeclaration, SyntaxFactory.Parse)
+
+        // SyntaxFactory.LocalDeclarationStatement();
+        // SyntaxFactory.AssignmentExpression(SyntaxKind.SimpleAssignmentExpression, SyntaxFactory.IdentifierName("query"),
+        //     SyntaxFactory.ObjectCreationExpression(SyntaxFactory.IdentifierName("_queryName")));
+        // Create a method
+        var routeParameters = EntityScheme.PrimaryKeys
+            .Select(x => SyntaxFactory
+                .Parameter(SyntaxFactory.Identifier(x.PropertyNameAsMethodParameterName))
+                .WithType(SyntaxFactory.ParseTypeName(x.TypeName)))
+            .ToArray();
+
+        var methodDeclaration = SyntaxFactory.MethodDeclaration(
+                SyntaxFactory.ParseTypeName("Task<IResult>"),
+                Scheme.Configuration.Endpoint.FunctionName)
+            .AddParameterListParameters(routeParameters)
+            .AddParameterListParameters([
+                SyntaxFactory
+                    .Parameter(SyntaxFactory.Identifier("queryDispatcher"))
+                    .WithType(SyntaxFactory.ParseTypeName("IQueryDispatcher")),
+                SyntaxFactory
+                    .Parameter(SyntaxFactory.Identifier("cancellation"))
+                    .WithType(SyntaxFactory.ParseTypeName(nameof(CancellationToken)))
+            ])
+            .AddModifiers([
+                SyntaxFactory.Token(SyntaxKind.PublicKeyword),
+                SyntaxFactory.Token(SyntaxKind.StaticKeyword),
+                SyntaxFactory.Token(SyntaxKind.AsyncKeyword),
+            ])
+            .WithBody(SyntaxFactory.Block(syntax));
+
+        // Add the field, the property and method to the class.
+        classDeclaration = classDeclaration.AddMembers(methodDeclaration);
+
+        // Add the class to the namespace.
+        @namespace = @namespace.AddMembers(classDeclaration);
+
+        compilationUnit = compilationUnit.AddMembers(@namespace);
+
+        // Normalize and get code as string.
+        var code = compilationUnit
+            .NormalizeWhitespace()
+            .ToFullString();
+
+        Context.AddSource($"{_endpointClassName}.g.cs", code);
     }
+
+    // private void GenerateEndpoint(string templatePath)
+    // {
+    //     var routeParams = EntityScheme.PrimaryKeys.FormatAsMethodDeclarationParameters();
+    //     var constructorParameters = EntityScheme.PrimaryKeys.FormatAsMethodCallArguments();
+    //     var model = new
+    //     {
+    //         EndpointClassName = _endpointClassName,
+    //         FunctionName = Scheme.Configuration.Endpoint.FunctionName,
+    //         QueryName = _queryName,
+    //         DtoName = _dtoName,
+    //         RouteParams = routeParams,
+    //         QueryConstructorParameters = constructorParameters
+    //     };
+    //
+    //     WriteFile(templatePath, model, _endpointClassName);
+    //
+    //     EndpointMap = new EndpointMap(EntityScheme.EntityName.ToString(),
+    //         Scheme.Configuration.OperationsSharedConfiguration.EndpointsNamespaceForFeature,
+    //         "Get",
+    //         Scheme.Configuration.Endpoint.Route,
+    //         $"{_endpointClassName}.{Scheme.Configuration.Endpoint.FunctionName}");
+    // }
 }
