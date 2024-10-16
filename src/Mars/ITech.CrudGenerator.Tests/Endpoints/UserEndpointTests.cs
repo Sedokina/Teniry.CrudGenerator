c875e91174812dfca69fdb63d9ad308684ea3eee
Sedokina <sedokina52@gmail.com>
2024-10-17 00:03:30 +0600

    Add all fields to user tests

--- src/Mars/ITech.CrudGenerator.Tests/Endpoints/UserEndpointTests.cs
+++ src/Mars/ITech.CrudGenerator.Tests/Endpoints/UserEndpointTests.cs
@@ -34,8 +34,24 @@ public class UserEndpointTests(TestApiFixture fixture)
         actual.Should().NotBeNull();
         actual!.Id.Should().Be(user.Id);
         actual.Name.Should().Be("Test User");
+        actual.Code.Should().Be('a');
+        actual.IsActive.Should().Be(true);
+        actual.RegistrationDate.Should().NotBeBefore(DateTime.Today.Date.ToUniversalTime());
+        actual.LastSignInDate.Should().NotBeBefore(DateTime.Today.Date.ToUniversalTime());
+        actual.ByteRating.Should().BeGreaterThan(0);
+        actual.ShortRating.Should().BeLessThan(0);
+        actual.IntRating.Should().BeLessThan(0);
+        actual.LongRating.Should().BeLessThan(0);
+        actual.SByteRating.Should().BeLessThan(0);
+        actual.UShortRating.Should().BeGreaterThan(0);
+        actual.UIntRating.Should().BeGreaterThan(0);
+        actual.ULongRating.Should().BeGreaterThan(0);
+        actual.FloatRating.Should().BeGreaterThan(0);
+        actual.DoubleRating.Should().BeGreaterThan(0);
+        actual.DecimalRating.Should().BeGreaterThan(0);
+        actual.NotId.Should().NotBeEmpty();
     }
-    
+
     [Theory]
     [InlineData("user?page=1&pageSize=10")]
     public async Task Should_GetUsersList(string endpoint)
@@ -58,10 +74,26 @@ public class UserEndpointTests(TestApiFixture fixture)
         actual.Items.Should().AllSatisfy(x =>
         {
             x.Id.Should().NotBeEmpty();
-            x.Name.Should().NotBeNullOrEmpty();
+            x.Name.Should().Be("Test User");
+            x.Code.Should().Be('a');
+            x.IsActive.Should().Be(true);
+            x.RegistrationDate.Should().NotBeBefore(DateTime.Today.Date.ToUniversalTime());
+            x.LastSignInDate.Should().NotBeBefore(DateTime.Today.Date.ToUniversalTime());
+            x.ByteRating.Should().BeGreaterThan(0);
+            x.ShortRating.Should().BeLessThan(0);
+            x.IntRating.Should().BeLessThan(0);
+            x.LongRating.Should().BeLessThan(0);
+            x.SByteRating.Should().BeLessThan(0);
+            x.UShortRating.Should().BeGreaterThan(0);
+            x.UIntRating.Should().BeGreaterThan(0);
+            x.ULongRating.Should().BeGreaterThan(0);
+            x.FloatRating.Should().BeGreaterThan(0);
+            x.DoubleRating.Should().BeGreaterThan(0);
+            x.DecimalRating.Should().BeGreaterThan(0);
+            x.NotId.Should().NotBeEmpty();
         });
     }
-    
+
     [Theory]
     [InlineData("user/create")]
     public async Task Should_CreateUser(string endpoint)
@@ -83,7 +115,7 @@ public class UserEndpointTests(TestApiFixture fixture)
         user.Should().NotBeNull();
         user!.Name.Should().Be("New User");
     }
-    
+
     [Theory]
     [InlineData("user/{0}/update")]
     public async Task Should_UpdateUser(string endpoint)
@@ -94,7 +126,26 @@ public class UserEndpointTests(TestApiFixture fixture)
         // Act
         var response = await _httpClient.PutAsJsonAsync(
             string.Format(endpoint, createdUser.Id),
-            new UpdateUserCommand(createdUser.Id) { Name = "Updated user name" });
+            new UpdateUserCommand(createdUser.Id)
+            {
+                Name = "Updated user name",
+                Code = 'b',
+                IsActive = false,
+                RegistrationDate = DateTime.UtcNow.AddDays(-1),
+                LastSignInDate = DateTimeOffset.UtcNow.AddDays(-1),
+                ByteRating = 13,
+                ShortRating = -95,
+                IntRating = -873198873,
+                LongRating = -17263715389164,
+                SByteRating = -9,
+                UShortRating = 95,
+                UIntRating = 873198873,
+                ULongRating = 17263715389164,
+                FloatRating = 28.54f,
+                DoubleRating = 87189.86378,
+                DecimalRating = 9813.7641635291m,
+                NotId = new Guid("8e358827-0a9d-4d02-9d07-a7265a76b5ae"),
+            });
         response.Should().FailIfNotSuccessful();
 
         // Assert correct response
@@ -104,8 +155,24 @@ public class UserEndpointTests(TestApiFixture fixture)
         var user = await _db.FindAsync<User>([createdUser.Id], new CancellationToken());
         user.Should().NotBeNull();
         user!.Name.Should().Be("Updated user name");
+        user.Code.Should().Be('b');
+        user.IsActive.Should().Be(false);
+        user.RegistrationDate.Should().BeCloseTo(DateTime.UtcNow.AddDays(-1), TimeSpan.FromMinutes(5));
+        user.LastSignInDate.Should().BeCloseTo(DateTimeOffset.UtcNow.AddDays(-1), TimeSpan.FromMinutes(5));
+        user.ByteRating.Should().Be(13);
+        user.ShortRating.Should().Be(-95);
+        user.IntRating.Should().Be(-873198873);
+        user.LongRating.Should().Be(-17263715389164);
+        user.SByteRating.Should().Be(-9);
+        user.UShortRating.Should().Be(95);
+        user.UIntRating.Should().Be(873198873);
+        user.ULongRating.Should().Be(17263715389164);
+        user.FloatRating.Should().BeApproximately(28.54f, 0.01f);
+        user.DoubleRating.Should().BeApproximately(87189.86378, 0.01f);
+        user.DecimalRating.Should().BeApproximately(9813.7641635291m, 0.01m);
+        user.NotId.Should().Be(new Guid("8e358827-0a9d-4d02-9d07-a7265a76b5ae"));
     }
-    
+
     [Theory]
     [InlineData("user/{0}/delete")]
     public async Task Should_DeleteUser(string endpoint)
@@ -128,7 +195,27 @@ public class UserEndpointTests(TestApiFixture fixture)
 
     private async Task<User> CreateUserAsync()
     {
-        var user = new User { Id = Guid.NewGuid(), Name = "Test User" };
+        var user = new User
+        {
+            Id = Guid.NewGuid(),
+            Name = "Test User",
+            Code = 'a',
+            IsActive = true,
+            RegistrationDate = DateTime.Today,
+            LastSignInDate = DateTimeOffset.Now,
+            ByteRating = 1,
+            ShortRating = -83,
+            IntRating = -19876718,
+            LongRating = -971652637891,
+            SByteRating = -4,
+            UShortRating = 83,
+            UIntRating = 19876718,
+            ULongRating = 971652637891,
+            FloatRating = 18.13f,
+            DoubleRating = 91873.862378,
+            DecimalRating = 867.97716829m,
+            NotId = new Guid("63c4e04c-77d3-4e27-b490-8f6e4fc635bd"),
+        };
         await _db.AddAsync(user);
         await _db.SaveChangesAsync();
         _db.ChangeTracker.Clear();
