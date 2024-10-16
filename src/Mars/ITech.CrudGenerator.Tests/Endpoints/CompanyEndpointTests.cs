a91cdb7b555ad395eb491e74c3d14eb41444120b
Sedokina <sedokina52@gmail.com>
2024-10-16 22:11:34 +0600

    Refactor endpoint tests

--- src/Mars/ITech.CrudGenerator.Tests/Endpoints/CompanyEndpointTests.cs
+++ src/Mars/ITech.CrudGenerator.Tests/Endpoints/CompanyEndpointTests.cs
@@ -10,59 +10,45 @@ using ITech.CrudGenerator.TestApi.Generators.CompanyGenerator;
 namespace ITech.CrudGenerator.Tests.Endpoints;
 
 [Collection("E2eTests")]
-public class CompanyEndpointTests
+public class CompanyEndpointTests(TestApiFixture fixture)
 {
-    private readonly TestMongoDb _db;
-    private readonly TestApiFixture _fixture;
-    private readonly HttpClient _httpClient;
-
-    public CompanyEndpointTests(TestApiFixture fixture)
-    {
-        _fixture = fixture;
-        _httpClient = _fixture.GetHttpClient();
-        _db = _fixture.GetDb();
-    }
+    private readonly TestMongoDb _db = fixture.GetDb();
+    private readonly HttpClient _httpClient = fixture.GetHttpClient();
 
     [Theory]
-    [InlineData("company/1dc25f7c-feb4-42e5-a1e0-2df2ee96be83")]
+    [InlineData("company/{0}")]
     public async Task Should_GetCompany(string endpoint)
     {
         // Arrange
-        var id = new Guid("1dc25f7c-feb4-42e5-a1e0-2df2ee96be83");
-        await _db.AddAsync(new Company { Id = id, Name = "Company to get" });
-        await _db.SaveChangesAsync();
-        _db.ChangeTracker.Clear();
-        
+        var createdCompany = await CreateCompany("Company to get");
+
         // Act
-        var response = await _httpClient.GetAsync(endpoint);
+        var response = await _httpClient.GetAsync(string.Format(endpoint, createdCompany.Id));
         response.Should().FailIfNotSuccessful();
 
         // Assert correct response
         response.StatusCode.Should().Be(HttpStatusCode.OK);
-        
+
         var actual = await response.Content.ReadFromJsonAsync<CompanyDto>();
         actual.Should().NotBeNull();
-        actual!.Id.Should().Be(id);
+        actual!.Id.Should().Be(createdCompany.Id);
         actual.Name.Should().Be("Company to get");
     }
-    
+
     [Theory]
     [InlineData("company?page=1&pageSize=10")]
     public async Task Should_GetCompaniesList(string endpoint)
     {
         // Arrange
-        var id = new Guid("ac637119-dcc9-4144-8d7a-38632d9fce27");
-        await _db.AddAsync(new Company { Id = id, Name = "Company to get one of list" });
-        await _db.SaveChangesAsync();
-        _db.ChangeTracker.Clear();
-        
+        await CreateCompany("Company to get one of list");
+
         // Act
         var response = await _httpClient.GetAsync(endpoint);
         response.Should().FailIfNotSuccessful();
 
         // Assert correct response
         response.StatusCode.Should().Be(HttpStatusCode.OK);
-        
+
         var actual = await response.Content.ReadFromJsonAsync<CompaniesDto>();
         actual.Should().NotBeNull();
         actual!.Page.PageSize.Should().BeGreaterThan(0);
@@ -96,50 +82,54 @@ public class CompanyEndpointTests
         company.Should().NotBeNull();
         company!.Name.Should().Be("My new company");
     }
-    
+
     [Theory]
-    [InlineData("company/90355ca9-d101-46a7-a694-a4c5a04b5015/update")]
+    [InlineData("company/{0}/update")]
     public async Task Should_UpdateCompany(string endpoint)
     {
         // Arrange
-        var id = new Guid("90355ca9-d101-46a7-a694-a4c5a04b5015");
-        await _db.AddAsync(new Company { Id = id, Name = "Company to update" });
-        await _db.SaveChangesAsync();
-        _db.ChangeTracker.Clear();
-        
+        var createdCompany = await CreateCompany("Company to update");
+
         // Act
-        var response =
-            await _httpClient.PutAsJsonAsync(endpoint, new UpdateCompanyCommand(id) { Name = "Updated company name" });
+        var response = await _httpClient.PutAsJsonAsync(
+            string.Format(endpoint, createdCompany.Id),
+            new UpdateCompanyCommand(createdCompany.Id) { Name = "Updated company name" });
         response.Should().FailIfNotSuccessful();
 
         // Assert correct response
         response.StatusCode.Should().Be(HttpStatusCode.NoContent);
 
         // Assert saved to db
-        var company = await _db.FindAsync<Company>([id], new CancellationToken());
+        var company = await _db.FindAsync<Company>([createdCompany.Id], new CancellationToken());
         company.Should().NotBeNull();
         company!.Name.Should().Be("Updated company name");
     }
-    
+
     [Theory]
-    [InlineData("company/a8b57bd8-81c6-446c-9cc4-39356ac3bd3d/delete")]
+    [InlineData("company/{0}/delete")]
     public async Task Should_DeleteCompany(string endpoint)
     {
         // Arrange
-        var id = new Guid("a8b57bd8-81c6-446c-9cc4-39356ac3bd3d");
-        await _db.AddAsync(new Company { Id = id, Name = "Company to delete" });
-        await _db.SaveChangesAsync();
-        _db.ChangeTracker.Clear();
+        var createdCompany = await CreateCompany("Company to delete");
 
         // Act
-        var response = await _httpClient.DeleteAsync(endpoint);
+        var response = await _httpClient.DeleteAsync(string.Format(endpoint, createdCompany.Id));
         response.Should().FailIfNotSuccessful();
 
         // Assert correct response
         response.StatusCode.Should().Be(HttpStatusCode.NoContent);
-        
+
         // Assert deleted from db
-        var company = await _db.FindAsync<Company>([id], new CancellationToken());
+        var company = await _db.FindAsync<Company>([createdCompany.Id], new CancellationToken());
         company.Should().BeNull();
+    }
+
+    private async Task<Company> CreateCompany(string companyName)
+    {
+        var company = new Company { Id = Guid.NewGuid(), Name = companyName };
+        await _db.AddAsync(company);
+        await _db.SaveChangesAsync();
+        _db.ChangeTracker.Clear();
+        return company;
     }
 }
