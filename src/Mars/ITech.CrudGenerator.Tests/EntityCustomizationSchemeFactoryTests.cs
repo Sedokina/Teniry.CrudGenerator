961addcdad9a9623c3d2e80077e79ef8df0d5cfc
Sedokina <sedokina52@gmail.com>
2024-11-18 22:33:00 +0600

    Create operation configuration tests

--- src/Mars/ITech.CrudGenerator.Tests/EntityCustomizationSchemeFactoryTests.cs
+++ src/Mars/ITech.CrudGenerator.Tests/EntityCustomizationSchemeFactoryTests.cs
@@ -1,3 +1,4 @@
+using System.Linq.Expressions;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using ITech.CrudGenerator.Abstractions.Configuration;
@@ -17,7 +18,7 @@ public class EntityCustomizationSchemeFactoryTests
     }
 
     [Fact]
-    public void Should_GetEntityNameFromSymbol()
+    public void Should_GetEntityTitleFromSymbol()
     {
         // Arrange
         var generator = CreateEntityGeneratorClass("Title = \"My custom title\";");
@@ -29,6 +30,74 @@ public class EntityCustomizationSchemeFactoryTests
         sut.Title.Should().Be("My custom title");
     }
 
+    [Fact]
+    public void Should_GetEntityPluralTitleFromSymbol()
+    {
+        // Arrange
+        var generator = CreateEntityGeneratorClass("TitlePlural = \"My custom title plural\";");
+
+        // Act
+        var sut = _factory.Construct(generator.Symbol, generator.Compilation);
+
+        // Assert
+        sut.TitlePlural.Should().Be("My custom title plural");
+    }
+
+    [Theory]
+    [InlineData("asc", "Prop")]
+    [InlineData("desc", "Prop")]
+    public void Should_GetDefaultSortFromSymbol(string direction, string propertyName)
+    {
+        // Arrange
+        var generator = CreateEntityGeneratorClass(
+            $"DefaultSort = new EntityGeneratorDefaultSort<MyCustomClass>(\"{direction}\", x => x.{propertyName});");
+
+        // Act
+        var sut = _factory.Construct(generator.Symbol, generator.Compilation);
+
+        // Assert
+        sut.DefaultSort.Should().NotBeNull();
+        sut.DefaultSort!.Direction.Should().Be(direction);
+        sut.DefaultSort.PropertyName.Should().Be(propertyName);
+    }
+
+    [Fact]
+    public void Should_GetEntityGeneratorCreateOperationConfigurationFromSymbol()
+    {
+        // Arrange
+        var generator = CreateEntityGeneratorClass(
+            @"CreateOperation = new EntityGeneratorCreateOperationConfiguration
+            {
+                Generate = true,
+                Operation = ""CustomOperation"",
+                OperationGroup = ""ManagedEntityCreateOperationCustomNs"",
+                CommandName = ""CustomizedNameCreateManagedEntityCommand"",
+                HandlerName = ""CustomizedNameCreateManagedEntityHandler"",
+                DtoName = ""CustomizedNameCreatedManagedEntityDto"",
+                GenerateEndpoint = true,
+                EndpointClassName = ""CustomizedNameCreateManagedEntityEndpoint"",
+                EndpointFunctionName = ""RunCreateAsync"",
+                RouteName = ""/customizedManagedEntityCreate""
+            };"
+        );
+
+        // Act
+        var sut = _factory.Construct(generator.Symbol, generator.Compilation);
+
+        // Assert
+        sut.CreateOperation.Should().NotBeNull();
+        sut.CreateOperation!.Generate.Should().BeTrue();
+        sut.CreateOperation.Operation.Should().Be("CustomOperation");
+        sut.CreateOperation.OperationGroup.Should().Be("ManagedEntityCreateOperationCustomNs");
+        sut.CreateOperation.CommandName.Should().Be("CustomizedNameCreateManagedEntityCommand");
+        sut.CreateOperation.HandlerName.Should().Be("CustomizedNameCreateManagedEntityHandler");
+        sut.CreateOperation.DtoName.Should().Be("CustomizedNameCreatedManagedEntityDto");
+        sut.CreateOperation.GenerateEndpoint.Should().BeTrue();
+        sut.CreateOperation.EndpointClassName.Should().Be("CustomizedNameCreateManagedEntityEndpoint");
+        sut.CreateOperation.EndpointFunctionName.Should().Be("RunCreateAsync");
+        sut.CreateOperation.RouteName.Should().Be("/customizedManagedEntityCreate");
+    }
+
     public (INamedTypeSymbol Symbol, CSharpCompilation Compilation) CreateEntityGeneratorClass(
         string ctorBody = "")
     {
@@ -37,7 +106,9 @@ public class EntityCustomizationSchemeFactoryTests
 using ITech.CrudGenerator.Abstractions.Configuration;
 
 namespace ITech.CrudGenerator.Tests {{
-    public class {className} {{ }}
+    public class {className} {{
+        public string Prop {{get; set;}}
+    }}
 
     public class {className}Generator : EntityGeneratorConfiguration<{className}> {{
         public {className}Generator() {{
@@ -49,6 +120,7 @@ namespace ITech.CrudGenerator.Tests {{
 
         var syntaxTree = CSharpSyntaxTree.ParseText(entityClass);
         var abstractions = MetadataReference.CreateFromFile(typeof(EntityGeneratorConfiguration<>).Assembly.Location);
+        var linqExpression = MetadataReference.CreateFromFile(typeof(Expression<>).Assembly.Location);
 
         var references = GetMetadataReferencesFromDllNames([
             "System.Private.CoreLib.dll",
@@ -58,7 +130,7 @@ namespace ITech.CrudGenerator.Tests {{
         var compilation = CSharpCompilation.Create(
             Assembly.GetExecutingAssembly().FullName,
             [syntaxTree],
-            references: [..references, abstractions],
+            references: [..references, abstractions, linqExpression],
             options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
         var err = compilation.GetDiagnostics();
         var symbol = compilation.GetSymbolsWithName($"{className}Generator").First();
