8e9ce10f0946dcbc1442b1344d7ece13a75e8dcd
Sedokina <sedokina52@gmail.com>
2024-12-15 16:44:22 +0600

    GetListQueryDefaultConfigurationBulderFactory tests

--- src/Mars/ITech.CrudGenerator/CrudGenerator.cs
+++ src/Mars/ITech.CrudGenerator/CrudGenerator.cs
@@ -71,7 +71,7 @@ public class CrudGenerator : ISourceGenerator
                     }
                 }
 
-                var getListConfiguration = GetListQueryDefaultConfigurationBulderFactory
+                var getListConfiguration = new GetListQueryDefaultConfigurationBulderFactory()
                     .Construct(
                         globalConfigurationBuilder,
                         sharedConfigurationBuilder,
