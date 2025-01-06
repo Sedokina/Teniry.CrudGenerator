9ff60ca8418bc1bd534d4cdadf003ccf9e68ed8f
Sedokina <sedokina52@gmail.com>
2025-01-06 12:15:23 +0600

    Add property builder

--- src/Mars/ITech.CrudGenerator/CrudGeneratorCore/OperationsGenerators/Core/ClassBuilder.cs
+++ src/Mars/ITech.CrudGenerator/CrudGeneratorCore/OperationsGenerators/Core/ClassBuilder.cs
@@ -187,3 +187,47 @@ internal class XmlDocException(string typeName, string description)
     public string TypeName { get; set; } = typeName;
     public string Description { get; set; } = description;
 }
+
+public class PropertyBuilder
+{
+    private PropertyDeclarationSyntax _property;
+
+    public PropertyBuilder(string fieldType, string fieldName)
+    {
+        _property = PropertyDeclaration(ParseTypeName(fieldType), fieldName)
+            .AddModifiers(Token(SyntaxKind.PublicKeyword))
+            .WithAccessorList(AccessorList(
+                List([
+                    AccessorDeclaration(SyntaxKind.GetAccessorDeclaration)
+                        .WithSemicolonToken(Token(SyntaxKind.SemicolonToken)),
+                    AccessorDeclaration(SyntaxKind.SetAccessorDeclaration)
+                        .WithSemicolonToken(Token(SyntaxKind.SemicolonToken))
+                ])));
+    }
+
+    public PropertyBuilder WithDefaultValue(string? defaultValue = null)
+    {
+        if (defaultValue is null)
+        {
+            return this;
+        }
+
+        _property = _property.WithInitializer(
+                // TODO: set actual default value, when it would not be "\"\""
+                EqualsValueClause(LiteralExpression(SyntaxKind.StringLiteralExpression, Literal("")))
+            )
+            .WithSemicolonToken(Token(SyntaxKind.SemicolonToken));
+        return this;
+    }
+    
+    public PropertyBuilder WithInheritDoc()
+    {
+        _property = _property.WithLeadingTrivia(ParseLeadingTrivia("/// <inheritdoc />\n"));
+        return this;
+    }
+
+    public PropertyDeclarationSyntax Build()
+    {
+        return _property;
+    }
+}
