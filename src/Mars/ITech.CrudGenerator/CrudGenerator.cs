b0a91f5c0a895553b99bf56891fd9cee1e2ad7d0
Sedokina <sedokina52@gmail.com>
2025-01-08 17:43:36 +0600

    Remove not used code

--- src/Mars/ITech.CrudGenerator/CrudGenerator.cs
+++ src/Mars/ITech.CrudGenerator/CrudGenerator.cs
@@ -26,24 +26,21 @@ public sealed class CrudGenerator : IIncrementalGenerator
             transform: (syntaxContext, _) => TransformFoundGeneratorConfigurationsToInternalScheme(syntaxContext)
         );
 
-        var dbContextSchemeProviders = context.SyntaxProvider.ForAttributeWithMetadataName(
-            typeof(UseDbContextAttribute).FullName ?? "",
-            predicate: (node, token) => true,
-            transform: (syntaxContext, token) => DbContextSchemeFactory.Construct(syntaxContext));
-
-        var generatorConfigurationsWithDbContextProviders =
-            generatorConfigurationsProviders.Combine(dbContextSchemeProviders.Collect());
-
-        var r = generatorConfigurationsProviders
-            .Select((configuration, token) => new EndpointMap(configuration.ClassMetadata.ClassName, "some", "things",
-                "gotta", "give", "well"))
-            .Collect();
+        var dbContextSchemeProviders = context.SyntaxProvider
+            .ForAttributeWithMetadataName(
+                typeof(UseDbContextAttribute).FullName ?? "",
+                predicate: (_, _) => true,
+                transform: (syntaxContext, _) => DbContextSchemeFactory.Construct(syntaxContext)
+            );
 
         List<EndpointMap> endpointsMaps = new();
         var globalConfigurationBuilder = GlobalCrudGeneratorConfigurationDefaultConfigurationFactory.Construct();
         var sharedConfigurationBuilder = new CqrsOperationsSharedConfigurationBuilderFactory().Construct();
 
         // TODO: check if there are dbContextSchemes at all
+        // TODO: add errors log
+        var generatorConfigurationsWithDbContextProviders =
+            generatorConfigurationsProviders.Combine(dbContextSchemeProviders.Collect());
         context.RegisterSourceOutput(generatorConfigurationsWithDbContextProviders,
             (productionContext, generatorConfigurationWithDbContext) =>
                 Execute(
@@ -56,7 +53,7 @@ public sealed class CrudGenerator : IIncrementalGenerator
                 )
         );
 
-        context.RegisterSourceOutput(r, (productionContext, maps) =>
+        context.RegisterSourceOutput(dbContextSchemeProviders.Collect(), (productionContext, _) =>
         {
             var mapEndpointsGenerator = new EndpointsMapGenerator(endpointsMaps, globalConfigurationBuilder);
             mapEndpointsGenerator.RunGenerator();
@@ -83,7 +80,7 @@ public sealed class CrudGenerator : IIncrementalGenerator
         return result;
     }
 
-    internal static void Execute(
+    private static void Execute(
         SourceProductionContext context,
         InternalEntityGeneratorConfiguration internalEntityGeneratorConfiguration,
         DbContextScheme dbContextScheme,
@@ -213,165 +210,6 @@ public sealed class CrudGenerator : IIncrementalGenerator
             }
         }
     }
-
-    // public void Initialize(GeneratorInitializationContext context)
-    // {
-    //     context.RegisterForSyntaxNotifications(() => new EntityGeneratorConfigurationSyntaxReceiver());
-    // }
-
-    // public void Execute(GeneratorExecutionContext context)
-    // {
-    //     try
-    //     {
-    //         if (context.SyntaxReceiver is not EntityGeneratorConfigurationSyntaxReceiver syntaxReceiver) return;
-    //
-    //         var dbContextScheme = DbContextSchemeFactory.Construct(context);
-    //
-    //         List<EndpointMap> endpointsMaps = new();
-    //         var globalConfigurationBuilder = GlobalCrudGeneratorConfigurationDefaultConfigurationFactory.Construct();
-    //         var sharedConfigurationBuilder = new CqrsOperationsSharedConfigurationBuilderFactory().Construct();
-    //
-    //         var internalEntityGeneratorConfigurationFactory = new InternalEntityGeneratorConfigurationFactory();
-    //         foreach (var classSyntax in syntaxReceiver.ClassesForCrudGeneration)
-    //         {
-    //             var (entityGeneratorConfigurationSymbol, entitySymbol) = classSyntax.AsSymbol(context);
-    //
-    //             var internalEntityGeneratorConfiguration = internalEntityGeneratorConfigurationFactory
-    //                 .Construct(entityGeneratorConfigurationSymbol as INamedTypeSymbol, context.Compilation);
-    //             var entityScheme = new EntitySchemeFactory().Construct(
-    //                 entitySymbol,
-    //                 internalEntityGeneratorConfiguration,
-    //                 dbContextScheme);
-    //
-    //             var getByIdQueryConfigurationBuilder = new GetByIdQueryDefaultConfigurationBuilderFactory()
-    //                 .Construct(
-    //                     globalConfigurationBuilder,
-    //                     sharedConfigurationBuilder,
-    //                     internalEntityGeneratorConfiguration.GetByIdOperation);
-    //
-    //             var getByIdQueryConfiguration = getByIdQueryConfigurationBuilder.Build(entityScheme);
-    //             if (getByIdQueryConfiguration.Generate)
-    //             {
-    //                 var getByIdQueryScheme =
-    //                     new CrudGeneratorScheme<CqrsOperationWithReturnValueGeneratorConfiguration>(
-    //                         entityScheme,
-    //                         dbContextScheme,
-    //                         getByIdQueryConfiguration);
-    //                 var generateGetByIdQuery = new GetByIdQueryCrudGenerator(getByIdQueryScheme);
-    //                 generateGetByIdQuery.RunGenerator();
-    //                 WriteFiles(context, generateGetByIdQuery.GeneratedFiles);
-    //                 if (generateGetByIdQuery.EndpointMap is not null)
-    //                 {
-    //                     endpointsMaps.Add(generateGetByIdQuery.EndpointMap);
-    //                 }
-    //             }
-    //
-    //             var getListConfiguration = new GetListQueryDefaultConfigurationBulderFactory()
-    //                 .Construct(
-    //                     globalConfigurationBuilder,
-    //                     sharedConfigurationBuilder,
-    //                     internalEntityGeneratorConfiguration.GetListOperation)
-    //                 .Build(entityScheme);
-    //             if (getListConfiguration.Generate)
-    //             {
-    //                 var getListQueryScheme = new CrudGeneratorScheme<CqrsListOperationGeneratorConfiguration>(
-    //                     entityScheme,
-    //                     dbContextScheme,
-    //                     getListConfiguration);
-    //                 var generateListQuery = new ListQueryCrudGenerator(getListQueryScheme);
-    //                 generateListQuery.RunGenerator();
-    //                 WriteFiles(context, generateListQuery.GeneratedFiles);
-    //                 if (generateListQuery.EndpointMap is not null)
-    //                 {
-    //                     endpointsMaps.Add(generateListQuery.EndpointMap);
-    //                 }
-    //             }
-    //
-    //             var createCommandConfiguration = new CreateCommandDefaultConfigurationBuilderFactory()
-    //                 .Construct(
-    //                     globalConfigurationBuilder,
-    //                     sharedConfigurationBuilder,
-    //                     internalEntityGeneratorConfiguration.CreateOperation)
-    //                 .Build(entityScheme);
-    //             if (createCommandConfiguration.Generate)
-    //             {
-    //                 var createCommandScheme =
-    //                     new CrudGeneratorScheme<CqrsOperationWithReturnValueGeneratorConfiguration>(
-    //                         entityScheme,
-    //                         dbContextScheme,
-    //                         createCommandConfiguration
-    //                     );
-    //                 var generateCreateCommand = new CreateCommandCrudGenerator(
-    //                     createCommandScheme,
-    //                     getByIdQueryConfiguration.Endpoint.Generate
-    //                         ? getByIdQueryConfigurationBuilder.Endpoint.RouteConfigurationBuilder
-    //                         : null,
-    //                     getByIdQueryConfiguration.Endpoint.Generate
-    //                         ? getByIdQueryConfigurationBuilder.OperationName
-    //                         : null);
-    //                 generateCreateCommand.RunGenerator();
-    //                 WriteFiles(context, generateCreateCommand.GeneratedFiles);
-    //                 if (generateCreateCommand.EndpointMap is not null)
-    //                 {
-    //                     endpointsMaps.Add(generateCreateCommand.EndpointMap);
-    //                 }
-    //             }
-    //
-    //             var updateOperationConfiguration = new UpdateCommandDefaultConfigurationBuilderFactory()
-    //                 .Construct(
-    //                     globalConfigurationBuilder,
-    //                     sharedConfigurationBuilder,
-    //                     internalEntityGeneratorConfiguration.UpdateOperation)
-    //                 .Build(entityScheme);
-    //             if (updateOperationConfiguration.Generate)
-    //             {
-    //                 var updateCommandScheme =
-    //                     new CrudGeneratorScheme<CqrsOperationWithReturnValueWithReceiveViewModelGeneratorConfiguration>(
-    //                         entityScheme,
-    //                         dbContextScheme,
-    //                         updateOperationConfiguration);
-    //                 var generateUpdateCommand = new UpdateCommandCrudGenerator(updateCommandScheme);
-    //                 generateUpdateCommand.RunGenerator();
-    //                 WriteFiles(context, generateUpdateCommand.GeneratedFiles);
-    //                 if (generateUpdateCommand.EndpointMap is not null)
-    //                 {
-    //                     endpointsMaps.Add(generateUpdateCommand.EndpointMap);
-    //                 }
-    //             }
-    //
-    //             var deleteCommandConfiguration = new DeleteCommandDefaultConfigurationBuilderFactory()
-    //                 .Construct(
-    //                     globalConfigurationBuilder,
-    //                     sharedConfigurationBuilder,
-    //                     internalEntityGeneratorConfiguration.DeleteOperation)
-    //                 .Build(entityScheme);
-    //             if (deleteCommandConfiguration.Generate)
-    //             {
-    //                 var deleteCommandScheme =
-    //                     new CrudGeneratorScheme<CqrsOperationWithoutReturnValueGeneratorConfiguration>(
-    //                         entityScheme,
-    //                         dbContextScheme,
-    //                         deleteCommandConfiguration);
-    //                 var generateDeleteCommand = new DeleteCommandCrudGenerator(deleteCommandScheme);
-    //                 generateDeleteCommand.RunGenerator();
-    //                 WriteFiles(context, generateDeleteCommand.GeneratedFiles);
-    //                 if (generateDeleteCommand.EndpointMap is not null)
-    //                 {
-    //                     endpointsMaps.Add(generateDeleteCommand.EndpointMap);
-    //                 }
-    //             }
-    //         }
-    //
-    //         var mapEndpointsGenerator = new EndpointsMapGenerator(endpointsMaps, globalConfigurationBuilder);
-    //         mapEndpointsGenerator.RunGenerator();
-    //         WriteFiles(context, mapEndpointsGenerator.GeneratedFiles);
-    //     }
-    //     catch (Exception e)
-    //     {
-    //         Logger.Print($"{e.Message}\n{e.StackTrace}");
-    //         Logger.FlushLogs(context);
-    //     }
-    // }
 
     private static void WriteFiles(SourceProductionContext context, List<GeneratorResult> files)
     {
---
+++ src/Mars/ITech.CrudGenerator/CrudGeneratorCore/Configurations/Operations/BuiltConfigurations/TypedBuiltConfigurations/TypeNamesForAnalyzers.cs
@@ -0,0 +1,6 @@
+namespace ITech.CrudGenerator.CrudGeneratorCore.Configurations.Operations.BuiltConfigurations.TypedBuiltConfigurations;
+
+internal static class TypeNamesForAnalyzers
+{
+    public const string EntityGeneratorDefaultSort = "EntityGeneratorDefaultSort";
+}
